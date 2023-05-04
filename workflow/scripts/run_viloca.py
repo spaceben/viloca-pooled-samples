a47992b0ed3ce7f76dfe3b2841d8612dc0a9b01c
Benjamin Langer <blanger@student.ethz.ch>
2023-05-04 20:52:39 +0200

    add: workflow complete

--- workflow/scripts/run_viloca.py
+++ workflow/scripts/run_viloca.py
@@ -1,22 +1,27 @@
 import subprocess
 
-def main(bam, merged_bam, ref, mode):
-    if mode == "classic":
-        bam = merged_bam
+def main(bam, ref, run):
+    l = run.split("_")
+    if len(l) == 2:
+        mode, id = l
     else:
+        mode = l[0]
+    if mode == "pooled":
         bam = " ".join(bam)
+    else:
+        bam = bam[0].replace("0", str(id)) # TODO dirty solution
 
+    print(bam)
     subprocess.run(f"shorah shotgun -b {bam} -f {ref} \
-                   --sampler use_quality_scores --alpha 0.0001 \
-                   --n_max_haplotypes 100", #--min_windows_coverage", # TODO min_windows_coverage
-                   cwd=f"results/viloca_{mode}", shell=True, check=True,
-                   capture_output=True)
+                    --sampler shorah --alpha 0.0001",
+                    #--n_max_haplotypes 100", #--min_windows_coverage", # TODO min_windows_coverage
+                    cwd=f"results/viloca_{run}", shell=True,
+                    check=True, capture_output=True)
 
 
 if __name__ == "__main__":
     main(
         [i.replace("results", "..") for i in snakemake.input.bam],
-        snakemake.input.merged_bam.replace("results", ".."),
         snakemake.input.ref.replace("results", ".."), # TODO better solution than replace?
-        snakemake.wildcards.mode
+        snakemake.wildcards.run
     )
